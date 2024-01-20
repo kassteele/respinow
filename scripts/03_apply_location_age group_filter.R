ba0a420ebf2b47c7d8e2a4e34658029bf774931a
kassteele <jan.vande.kassteele@rivm.nl>
2024-01-20 13:45:39 +0100

    added icosari/sari age group nowcasts

--- scripts/03_apply_location_age group_filter.R
+++ scripts/03_apply_location_age group_filter.R
@@ -18,26 +18,17 @@
 #       any_data = c_across(cols = starts_with("value")) |> any(),
 #       .keep = "unused")
 # }
-# 
+#
 # # Show if there is any data for locations and age groups
 # # Provides information to filter on
 # data_icosari_sari_raw |> fun_any_data()
 # data_survstat_influenza_raw |> fun_any_data()
-# data_survstat_pneu_raw |> fun_any_data()
 # data_survstat_rsv_raw |> fun_any_data()
 
-# Only information for DE 00+
-data_icosari_sari_raw <- data_icosari_sari_raw |> 
-  filter(
-    age_group == "00+")
-
 # As is
+data_icosari_sari_raw <- data_icosari_sari_raw
 data_survstat_influenza_raw <- data_survstat_influenza_raw
-
-# # As is
-# data_survstat_pneu_raw <- data_survstat_pneu_raw
-
 # Drop location DE-SN, because this is currently the only state
-data_survstat_rsv_raw <- data_survstat_rsv_raw |> 
+data_survstat_rsv_raw <- data_survstat_rsv_raw |>
   filter(
     location != "DE-SN")
