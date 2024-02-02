d121f9421269e9c5c4ea89e7f95361d21e1c4e38
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2024-02-02 09:31:28 +0100

    print info about locations and age groups

--- scripts/03_apply_location_age group_filter.R
+++ scripts/03_apply_location_age group_filter.R
@@ -2,28 +2,29 @@
 # Apply location and age group filter
 #
 
-# # Helper function that shows if there is any data
-# # on location and age group
-# fun_any_data <- function(data) {
-#   data |>
-#     group_by(
-#       location, age_group) |>
-#     summarise(
-#       across(
-#         .cols = starts_with("value"),
-#         .fns = \(x) (!is.na(x) & x != 0) |> any()),
-#       .groups = "drop") |>
-#     rowwise() |>
-#     mutate(
-#       any_data = c_across(cols = starts_with("value")) |> any(),
-#       .keep = "unused")
-# }
-#
-# # Show if there is any data for locations and age groups
-# # Provides information to filter on
-# data_icosari_sari_raw |> fun_any_data()
-# data_survstat_influenza_raw |> fun_any_data()
-# data_survstat_rsv_raw |> fun_any_data()
+# Helper function that shows if there is any data
+# on location and age group
+fun_any_data <- function(data) {
+  data_name <- data |> substitute() |> deparse()
+  data |>
+    group_by(
+      data_name = data_name, location, age_group) |>
+    summarise(
+      across(
+        .cols = starts_with("value"),
+        .fns = \(x) (!is.na(x) & x != 0) |> any()),
+      .groups = "drop") |>
+    rowwise() |>
+    mutate(
+      any_data = c_across(cols = starts_with("value")) |> any(),
+      .keep = "unused")
+}
+
+# Show if there is any data for locations and age groups
+# Provides information to filter on
+data_icosari_sari_raw |> fun_any_data()
+data_survstat_influenza_raw |> fun_any_data()
+data_survstat_rsv_raw |> fun_any_data()
 
 # As is
 data_icosari_sari_raw <- data_icosari_sari_raw
