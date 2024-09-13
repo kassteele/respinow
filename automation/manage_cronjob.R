22fedaa15a4b18c5adc23b8b6588f5e7009d49b1
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2024-09-13 10:10:31 +0200

    Debug cronjob management

--- automation/manage_cronjob.R
+++ automation/manage_cronjob.R
@@ -4,17 +4,25 @@
 
 # Load packages
 library(cronR)
+library(stringr)
+
+# Path of the automation script
+# Get user home dir, because ~ will not work from the Linux command line
+user_home <- Sys.getenv("HOME")
+automation_script <- file.path(user_home, "Surfdrive/R_topics/nowcasting/Github_repositories/respinow/automation/automated_submission.R")
+automation_dir <- dirname(automation_script)
 
 # Add cron job
-# Log will be added in same dir as automated_submission.R
+# The workdir will be set one dir higher than automation_dir
+# This is the project directory
 cron_add(
   command = cron_rscript(
-    rscript = "~/Surfdrive/R_topics/nowcasting/Github_repositories/respinow/automation/automated_submission.R",
-    workdir = "~/Surfdrive/R_topics/nowcasting/Github_repositories/respinow",
+    rscript = automation_script,
+    workdir = dirname(automation_dir),
     log_append = FALSE),
   frequency = "daily",
   days_of_week = 5,
-  at = "9:00",
+  at = "10:00",
   id = "respinow",
   ask = FALSE)
 
