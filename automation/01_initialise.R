de5d5b2db482865b93bc9d21f0cd3e8f3e7fa521
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2024-10-04 14:48:02 +0200

    comments added to folder names

--- automation/01_initialise.R
+++ automation/01_initialise.R
@@ -6,8 +6,8 @@
 library(stringr)
 
 # Set names
-upstream_user_name <- "KITmetricslab"
-origin_user_name <- "kassteele"
-repos_name <- "RESPINOW-Hub"
-local_folder <- "~/Documents"
-project_folder <- getwd()
+upstream_user_name <- "KITmetricslab" # This is the hub's GitHub name
+origin_user_name <- "kassteele" # This is your GitHub name
+repos_name <- "RESPINOW-Hub" # This is the name of the hub's repository
+local_folder <- "~/Projects" # This is where the repository's clone is
+project_folder <- getwd() # This is where the R scripts are (within the RStudio Project)
